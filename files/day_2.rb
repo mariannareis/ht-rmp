54b91b67b00bdd82ad5040f50286ef9ab30ee0c7
mariannareis <mari.ounaum@gmail.com>
2011-02-03 16:06:52 -0200

    Day 2: all exercises passing in test, but I have to review

--- files/day_2.rb
+++ files/day_2.rb
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 #1
 def send_message
   m = Message.new()
@@ -31,7 +33,60 @@ class Song
 
 end
 
-#class Object
-  #Song = Class.new
-#end
+#6
+class String
+  def to_leet
+    hash = {"a" => "4", "e" => "3", "i" => "1", "o" => "0", "u" => "μ" }
+    self.each_char do |letra|
+      self[letra] = hash[letra] if hash[letra] != nil
+   end
+  end
+end
+
+#7 e 8
+class Object
+  attr_accessor :blog
+
+  def reverse_attr(arg1, arg2)
+  end
+
+  def leet_attr(arg1, arg2)
+  end
+end
+
+Object.class_eval do
+  %w[blog].each do |blog|
+    define_method(blog.to_leet) do |*args|
+      return self.blog.to_leet
+    end
+  end
+end
+
+Object.class_eval do
+  %w[blog].each do |method_name|
+    define_method(method_name.reverse) do |*args|
+      return self.blog.reverse
+    end
+  end
+end
+
+#9
+Car.singleton_class.class_eval { undef_method :useless }
+
+#10
+class Ferrari
+  def color(default_color = "black")
+    @color = default_color
+  end
+end
+
+#11
+Ferrari.class_eval { undef_method :engine }
+
+module Calc
+  def self.sum(arg1, arg2)
+    arg1 = 0 if arg1 == nil
+    arg1 + arg2
+  end
+end
 
